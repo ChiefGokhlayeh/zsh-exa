bbf4532745f79667b76065c7baae575706a3c46a
Andreas Baulig <free.geronimo@hotmail.de>
2023-09-16 15:16:09 +0200

    Make plugin compatible with eza

--- zsh-exa.plugin.zsh
+++ zsh-exa.plugin.zsh
@@ -1,17 +1,22 @@
-# Oh My ZSH - exa aliases plugin
+# Oh My ZSH - exa/eza aliases plugin
+
+# eza is a community fork of exa and now the default on Arch
+exa_cmd='exa'
 
 # Check if exa is installed, give a warning if it isn't and abort
 if ! (( $+commands[exa] )); then
   print "zsh-exa-plugin: Warning, exa was not found. Please install exa to use this plugin.." >&2
   return 1
+elif (( $+commands[eza] )); then
+  exa_cmd='eza'
 fi
 
 # Set time to bold
 #export EXA_COLORS='da=1;34:gm=1;34'
 
 # Group directories first, show icons, and enable file size color scale
-alias ls='exa --group-directories-first --icons --color-scale' # --time-style=iso
-alias lt='exa --tree --level=2 --icons' # Show in tree view
+alias ls="${exa_cmd} --group-directories-first --icons --color-scale" # --time-style=iso
+alias lt="${exa_cmd} --tree --level=2 --icons" # Show in tree view
 alias l='ls -a'                         # Short, all files
 alias ld='l -D'                         # Short, only directories
 alias ll='ls -lbg --git'                # Long, file size prefixes, grid, git status
@@ -20,3 +25,5 @@ alias lC='la --sort=changed'            # Long, sort changed
 alias lM='la --sort=modified'           # Long, sort modified
 alias lS='la --sort=size'               # Long, sort size
 alias lX='la --sort=extension'          # Long, sort extension
+
+unset exa_cmd
